93dc607488ac42ca9866df6789332236c7fb34d9
unknown <danoh82@gmail.com>
2018-02-16 22:46:07 -0800

    very basic setup

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import Home from './Components/Home';
+import Header from './Components/Header';
+import About from './Components/About';
+import Portfolio from './Components/Portfolio';
+import Contact from './Components/Contact';
 class App extends Component {
   render() {
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <Home />
+          <Header />
+          <About />
+          <Portfolio/>
+          <Contact />
       </div>
     );
   }
